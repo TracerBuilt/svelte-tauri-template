cec727e88a69144caaa5c8f0fb9dbd2371c6bdbb
Tyler Ward <tyler.t.ward@icloud.com>
2022-07-23 22:31:01 -0500

    add Storybook

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
+import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({
-		postcss: true,
+		postcss: true
 	}),
 
 	kit: {
 		adapter: adapter()
-	}
-};
+	},
 
-export default config;
+	vite: {
+		resolve: {
+			alias: {
+				$lib: path.resolve('./src/lib'),
+				$components: path.resolve('./src/lib/components')
+			}
+		}
+	}
+}
+
+export default config
